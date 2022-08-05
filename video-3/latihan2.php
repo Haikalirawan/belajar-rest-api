30a071541e89c0b9c8874e116605ef407a79c2fc
Likar <haikalirawan5@gmail.com>
2022-08-05 22:21:38 +0700

    Belajar REST-API tanggal 5 agustus

---
+++ video-3/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+
+// MENGUBAH JSON KE ARRAY ASSOSIATIF
+
+$data = file_get_contents('coba.json');
+$mahasiswa = json_decode($data, true);
+
+var_dump($mahasiswa);
+
+echo $mahasiswa[0]["DPA"]["DPA1"];
